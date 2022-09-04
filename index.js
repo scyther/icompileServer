ff6aa100541edceefead3757cdf36ba1d2d60a6f
Chirag Sharma <sharma.chirag3737@gmail.com>
2022-09-04 12:27:39 +0530

    code reformatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path')
+const path = require("path");
 const { generateFile } = require("./generateFile");
 const { executeC } = require("./executeC");
 const { executeCpp } = require("./executeCpp");
@@ -12,21 +12,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
-// All other GET requests not handled before will return our React app
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
-
-
 
 app.post("/run", async (req, res) => {
   //getting the required data from the request
@@ -55,6 +47,10 @@ app.post("/run", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || PORT, () => {
